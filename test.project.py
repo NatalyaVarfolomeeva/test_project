9decba9fcc43676218025b98b7d69aa1ec1d316c
Natalya Varfolomeeva <nat@adm-partner.ru>
2024-02-15 09:47:05 +0300

    Add try and except

--- test.project.py
+++ test.project.py
@@ -2,10 +2,15 @@ print('Это пробная версия для отправки на GIT')
 x_list = []
 y_list = []
 for i in range(10):
-    x = int(input('Введите X = '))
-    x_list.append(x)
-    y = int(input('Введите Y = '))
-    y_list.append(y)
+    try:
+        x = int(input('Введите X = '))
+        x_list.append(x)
+        y = int(input('Введите Y = '))
+        y_list.append(y)
+    except:
+        ValueError
+        print('Нужно вводить только цифры.')
+        print('Попробуем еще раз.')
 
 oper_dir_x = [x ** 3 for x in x_list]
 print(oper_dir_x)

b72a4e9596e408e946657ca5ad22daee7913f783
Natalya Varfolomeeva <nat@adm-partner.ru>
2024-02-15 09:27:59 +0300

    Add list

--- test.project.py
+++ test.project.py
@@ -1,1 +1,11 @@
 print('Это пробная версия для отправки на GIT')
+x_list = []
+y_list = []
+for i in range(10):
+    x = int(input('Введите X = '))
+    x_list.append(x)
+    y = int(input('Введите Y = '))
+    y_list.append(y)
+
+oper_dir_x = [x ** 3 for x in x_list]
+print(oper_dir_x)
